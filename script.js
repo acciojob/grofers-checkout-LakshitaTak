dd97a873dd6f470dd87b02b1c893903e1f64c09e
LakshitaTak <134226199+LakshitaTak@users.noreply.github.com>
2023-09-26 15:10:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,30 +3,17 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-  // Select all elements with class names "item" and "price"
-  const items = document.querySelectorAll(".item");
-  const prices = document.querySelectorAll(".price");
+  //Add your code here
+	
+  let sum = 0;
+  document.querySelectorAll("td.price").forEach((p) => {
+	  sum += parseInt(p.textContent.trim())
+  })
 
-  let totalPrice = 0;
+	const tr = document.createElement("tr");
+	tr.innerHTML = `<td id="ans">${sum}</td>`
 
-  // Calculate the total price by summing up the individual prices
-  for (let i = 0; i < items.length; i++) {
-    totalPrice += parseFloat(prices[i].textContent);
-  }
-
-  // Create a new row for displaying the total price
-  const totalRow = document.createElement("tr");
-  const totalCell1 = document.createElement("td");
-  totalCell1.textContent = "Total Price";
-  const totalCell2 = document.createElement("td");
-  totalCell2.textContent = totalPrice;
-
-  totalRow.appendChild(totalCell1);
-  totalRow.appendChild(totalCell2);
-
-  // Append the new row to the table
-  const table = document.querySelector("table");
-  table.appendChild(totalRow);
+	document.querySelector("table").appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
